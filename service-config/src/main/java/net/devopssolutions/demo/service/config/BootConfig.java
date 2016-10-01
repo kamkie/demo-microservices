e3876c70423e75c0a09bc252e763599e5a5af05e
Kamil Kiewisz <kamkie@outlook.com>
2016-10-01 20:18:56 +0200

    init first service

---
+++ service-config/src/main/java/net/devopssolutions/demo/service/config/BootConfig.java
@@ -0,0 +1,12 @@
+package net.devopssolutions.demo.service.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootConfig {// NOSONAR
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootConfig.class, args);// NOSONAR
+    }
+}
