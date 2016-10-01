bdb9c3436e8c4f51a5ec7e535d1368f224285f57
Kamil Kiewisz <kamkie@outlook.com>
2016-10-02 00:09:43 +0200

    sonar with coverage

---
+++ common-filters/src/test/java/net/devopssolutions/demo/common/filter/TimeLoggingFilterTest.java
@@ -0,0 +1,13 @@
+package net.devopssolutions.demo.common.filter;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TimeLoggingFilterTest {
+    @Test
+    public void doFilterInternal() throws Exception {
+        assertThat(true).isTrue();
+    }
+
+}
--- service-config/src/test/java/net/devopssolutions/demo/service/config/ServiceConfigTests.java
+++ service-config/src/test/java/net/devopssolutions/demo/service/config/ServiceConfigTests.java
@@ -2,15 +2,35 @@ package net.devopssolutions.demo.service.config;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ServiceConfigTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void home() throws Exception {
+        ResponseEntity<String> responseEntity = this.restTemplate.getForEntity("/management/info", String.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.hasBody()).isTrue();
+        assertThat(responseEntity.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON_UTF8);
+        assertThat(responseEntity.getBody()).contains("microservices");
     }
 
 }
